c293c283226819d9371793e55673330f0eafbe50
于金祥 <1028920761@qq.com>
2015-07-25 11:08:57 +0800

    Revert "ertefa"
    This reverts commit b8eb40766c80b9aa5c416099afd84ce2d36aa6ca.

---
+++ BuerShoppingforsell/BuerShoppingforsell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuerShoppingforsell
+//
+//  Created by 于金祥 on 15/7/25.
+//  Copyright (c) 2015年 zykj.BuerShoppingforsell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BuerShoppingforsell/BuerShoppingforsell/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BuerShoppingforsell
+//
+//  Created by 于金祥 on 15/7/25.
+//  Copyright (c) 2015年 zykj.BuerShoppingforsell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
