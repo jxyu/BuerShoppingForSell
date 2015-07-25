3885367e3b3447461dfb712aaaeed9b1c8e38e5c
于金祥 <1028920761@qq.com>
2015-07-25 11:04:53 +0800

    jianlixiangmu

---
+++ BuerShoppingforsell/BuerShoppingforsell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuerShoppingforsell
+//
+//  Created by 于金祥 on 15/7/25.
+//  Copyright (c) 2015年 zykj.BuerShoppingforsell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
